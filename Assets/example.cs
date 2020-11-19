ed0f4075227d536d132d6a42f7e4a3e2c360ef82
AlexanderKing\Alexander King <alexander@akingsquest.com>
2020-11-19 10:38:29 -0500

    Revert "Changed the example script"
    This reverts commit 059f6d26c24664b41029cf4d1223981062400133.

--- Assets/example.cs
+++ Assets/example.cs
@@ -7,8 +7,7 @@ public class example : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		//TODO: make this script do things.
-		int a = 1;
+        
     }
 
     // Update is called once per frame
