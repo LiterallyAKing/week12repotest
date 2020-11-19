059f6d26c24664b41029cf4d1223981062400133
AlexanderKing\Alexander King <alexander@akingsquest.com>
2020-11-19 10:26:16 -0500

    Changed the example script

--- Assets/example.cs
+++ Assets/example.cs
@@ -7,7 +7,8 @@ public class example : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+		//TODO: make this script do things.
+		int a = 1;
     }
 
     // Update is called once per frame
